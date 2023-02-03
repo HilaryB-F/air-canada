1229788918f26e59b6cff4621e023978941fa98b
Hiu Suet Cheung <ch.suet@hotmail.com>
2023-02-02 21:03:47 -0500

    add quiz2 page

--- src/pages/Quiz2.js
+++ src/pages/Quiz2.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import "./Quiz1.scss";
+import { Link } from "react-router-dom";
+import Asos from "../assets/images/asos-button.png";
+import Ssense from "../assets/images/ssense-button.png";
+import Arrow from "../assets/images/arrow.png";
+import Dots from "../assets/images/dots2.png";
 
 export default function Quiz2() {
   return (
-    <div>
-      
+    <div className="main__container">
+      <div className="main__back">
+        <img className="back" alt="back" src={Arrow}></img>
+      </div>
+
+      <div className="quiz__info">
+        <div className="quiz__text">
+          <p className="quiz__words">Tap to select...</p>
+
+          <div className="quiz__choices">
+            <img className="quiz__choice1" alt="Choice1" src={Asos}></img>
+            <img className="quiz__choice2" alt="Choice2" src={Ssense}></img>
+          </div>
+        </div>
+
+        <div className="quiz__question">
+          <h2 className="question">Where do you prefer to shop for fashion?</h2>
+
+          <div className="quiz__nav">
+            <Link className="quiz__nav-back" to="/quiz1">
+              Go back
+            </Link>
+            <Link className="quiz__nav-forward" to="/quiz3">
+              Skip question
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <img className="dots" alt="Dots" src={Dots}></img>
     </div>
-  )
+  );
 }
